8765c91282935066fc15675f445c25c1a18ef92b
Melissa Lee <plhw90@gmail.com>
2021-01-12 11:04:16 -0700

    Initial Commit

---
+++ Enroute/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Enroute
+//
+//  Created by 이향원 on 2021-01-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
